ef2220bf754713fe36e7742d125e9656cfbc16d7
Muniraj97 <muniram14meiet92@gmail.com>
2025-03-16 21:17:46 +0530

    muni

---
+++ src/org/set/Employee.java
@@ -0,0 +1,47 @@
+package org.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+public class Employee {
+public static void main(String[] args) {
+	Set r=new HashSet();
+	r.add(10);
+	r.add("java");
+	r.add(55);
+	r.add(34.234f);
+	r.add('m');
+	r.add(500);
+	r.add(10);
+	r.add(null);
+	System.out.println(r);
+	
+	
+	Set r1=new LinkedHashSet();
+	r1.add(10);
+	r1.add("java");
+	r1.add(55);
+	r1.add(34.234f);
+	r1.add('m');
+	r1.add(500);
+	r1.add(10);
+	r1.add(null);
+	System.out.println(r1);
+	Set<Integer> r2=new TreeSet();
+	r2.add(10);
+	r2.add(3);
+	r2.add(30);
+	r2.add(400);
+	r2.add(35);
+	System.out.println(r2);
+	//size
+	int size = r.size();
+	System.out.println(size);
+	boolean contains = r.contains(null);
+	System.out.println(contains);
+	
+	
+	
+}
+}
